21b648d8ba329a649e1c65bf864e8f0167d0ab97
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-07-03 19:27:41 +0300

    connect the project to the td

--- app.ts
+++ app.ts
@@ -15,5 +15,5 @@ app.get('/users', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server isconected and  running on port ${PORT}`);
 });
