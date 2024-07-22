fa3ba77b7f1f8383d876ee0111427da497702156
friedmanrivka <rivkaf291@gmail.com>
2024-07-22 15:37:14 +0300

    the project is ok

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ import ProductManageRoutes from './src/routes/productManagerRoutes';
 import quarterDatesRoutes from './src/routes/quarterDatesRoutes';
 import finalDesicion from './src/routes/filnalDesicionRoutes'
 import statusRoutes from './src/routes/statusRoutes'
-//import newRequest from './src/routes/newRequestRouter';';
+import newRequest from './src/routes/newRequestRouter';
 import AuthenticationRoutes  from './src/routes/AuthenticationRoutes'
 import cors from 'cors';
 
@@ -26,6 +26,7 @@ app.use('/api', quarterDatesRoutes);
 app.use('/api',finalDesicion);
 app.use('/api',statusRoutes);
 app.use('/api',AuthenticationRoutes);
+app.use('/api',newRequest);
 
 
 app.listen(port, () => {
--- src/controllers/requestController.ts
+++ src/controllers/requestController.ts
@@ -2,22 +2,6 @@ import { Request, Response } from 'express';
 import RequestRepo from '../repositories/requestRepo';
 import {} from '../models/extendedRequestModel'
 
-
-
-// export const getAllRequst = async (req: Request, res: Response): Promise<void> => {
-//     console.log('controller');
-//     try {
-//         const descriptions = await RequestRepo.getAllRequest();
-//         res.json(descriptions);
-//     } catch (error) {
-//         console.error('Error fetching descriptions:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// };
-// import { Request, Response } from 'express';
-// import RequestRepo from '../repositories/requestRepo';
-// import {} from '../models/extendedRequestModel';
-
 export const getAllRequest = async (req: Request, res: Response): Promise<void> => {
     console.log('Controller: Entering getAllRequest method');
     try {
@@ -32,8 +16,8 @@ export const getAllRequest = async (req: Request, res: Response): Promise<void> 
 
 export const updateFinalDecision = async (req: Request, res: Response): Promise<void> => {
     try {
-        const id = parseInt(req.params.id, 10); // Ensure id is parsed as a number
-        const finalDecision = req.body.finalDecision; // Ensure finalDecision is parsed as a number
+        const id = parseInt(req.params.id, 10); 
+        const finalDecision = req.body.finalDecision; 
         if (isNaN(id) ) {
             res.status(400).send('Invalid id or finalDecision');
             return;
