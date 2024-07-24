6fc97e11551a868f6e3561334e4e6362e8a41107
friedmanrivka <rivkaf291@gmail.com>
2024-07-24 16:28:49 +0300

    export the table to csv file

--- src/routes/exportRoutes.ts
+++ src/routes/exportRoutes.ts
@@ -1,13 +1,4 @@
 import { Router } from 'express';
-// // import { exportToGoogleSheets } from '../services/exportService';
-
-// const router = Router();
-
-// // router.get('/export', exportToGoogleSheets);
-
-// export default router;
-
-
 import { exportToCSV } from '../services/exportService';
 
 const router = Router();
