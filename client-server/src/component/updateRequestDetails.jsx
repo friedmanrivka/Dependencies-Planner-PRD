ab1cdaddcd4de17115c0eee533ad42d0894f0542
yehuditPeretz <ditiprz@gmail.com>
2024-07-25 15:29:24 +0300

    udate final desition

---
+++ client-server/src/component/updateRequestDetails.jsx
@@ -0,0 +1,13 @@
+const UdateRquest = ({finalDecisionChose}) => {
+    console.log("finalDecision"+finalDecisionChose)
+    const handleUdateFinalDecision = async () => {
+       
+    };
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default UdateRquest;
