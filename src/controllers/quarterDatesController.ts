6a90b6938c2c089095996bf6f1ab264f142434bd
friedmanrivka <rivkaf291@gmail.com>
2024-08-06 00:39:34 +0300

    add funtiom get to recive just the current date

--- src/controllers/quarterDatesController.ts
+++ src/controllers/quarterDatesController.ts
@@ -18,13 +18,28 @@ export const setCurrentQuarter = async (req: Request, res: Response) => {
 export const getCurrentQuarter = async (req: Request, res: Response) => {
   try {
     const currentQuarters = await QuarterRepo.getCurrentQuarter();
-    if (currentQuarters.length === 0) {
-      return res.status(404).json({ message: 'No current quarter found.' });
+    if (!currentQuarters || currentQuarters.length === 0) {
+      return res.status(404).json({ message: 'No current quarters found.' });
     }
-    
-    return res.status(200).json(currentQuarters);
+    return res.status(200).json({ currentQuarters }); // Return as a JSON object with a key
   } catch (error) {
     console.error('Controller: Error fetching current quarters:', error);
     return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const getQ = async (req: Request, res: Response) => {
+  try {
+    const currentQuarter = await QuarterRepo.getQ();
+
+    if (!currentQuarter) {
+      return res.status(404).send('No current quarter found.');
+    }
+
+    // Return the current quarter as a plain string
+    return res.status(200).send(currentQuarter);
+  } catch (error) {
+    console.error('Controller: Error fetching current quarter:', error);
+    return res.status(500).send('Internal server error');
   }
 };
--- src/routes/quarterDatesRoutes.ts
+++ src/routes/quarterDatesRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import {  setCurrentQuarter,getCurrentQuarter } from '../controllers/quarterDatesController';
+import {  setCurrentQuarter,getCurrentQuarter,getQ} from '../controllers/quarterDatesController';
 
 
 const router = Router();
 router.post('/set-current-quarter', setCurrentQuarter);
 router.get('/get-current-quarter', getCurrentQuarter);
+router.get('/getQ', getQ);
+
 
 export default router;
